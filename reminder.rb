d87ab70a7e0ff325b417df1934ac4005de25ef3c
Paul McKellar <paul.mckellar@gmail.com>
2012-11-16 15:26:24 -0800

    gamifing my personal console

--- reminder.rb
+++ reminder.rb
@@ -48,16 +48,100 @@ class AliasSuggestor
   end
 end
 
+class CommandSuccessCounter
+
+  require 'fileutils'
+  require 'yaml'
+
+  @@default_file_location = "~/.realias_success_metrics"
+
+  @@positive_points = 1
+  @@negative_points = 50
+
+  attr_accessor :count, :data, :path
+
+  def initialize
+    if !file_exists?
+      @data = {}
+      self.write
+    else
+      self.load
+    end
+    set_defaults!
+  end
+
+  def count
+    self.data[:count]
+  end
+
+  def high_score
+    self.data[:high_score]
+  end
+
+  def increment!
+    self.data[:count] = self.data[:count] + @@positive_points
+    mark_high_score
+    self.write
+  end
+
+  def mistake!
+    self.data[:count] = [self.data[:count] - @@negative_points, 0].max
+    self.write
+  end
+
+  def mark_high_score
+    return unless high_score?
+    self.data[:high_score] = count
+    self.data[:high_score_at] = Time.now
+  end
+
+protected
+
+  def high_score?
+    count > high_score
+  end
+
+  def set_defaults!
+    @data[:count] ||= 0
+    @data[:high_score] ||= 0
+    @data[:high_score_at] ||= Time.now
+  end
+
+  def load
+    self.data = YAML.load(File.new(path, 'r').read)
+  end
+
+  def write
+    file = File.new(path, 'w')
+    file << data.to_yaml
+    file.close
+  end
+
+  def path
+    File.expand_path(@@default_file_location)
+  end
+
+  def touch
+    FileUtils.touch(path)
+  end
+
+  def file_exists?
+    File.exists?(path)
+  end
+
+end
+
+csc = CommandSuccessCounter.new
+
 path = File.expand_path("~/.alias.cache")
 reader = AliasSuggestor.new(path)
-
-# exit if ARGF.nil?
 
 input = ARGF.read.strip
 suggested = reader.suggest(input)
 if suggested.nil?
-  # puts "Can't improve! '#{input}' '#{suggested}'"
+  csc.increment!
+  puts "#{csc.count}"
 else
-  # puts "Needs work! '#{input}' '#{suggested}'"
-  puts suggested
+  csc.mistake!
+  puts "#{suggested} (#{csc.count})"
 end
